5b272ab2ceb791cd666cf18227ce1aa172da84fc
gehringerhugo <hugo.gehringer@free.fr>
2024-03-29 12:03:06 +0100

    mongodb crud method

--- ws_crud_mongo.js
+++ ws_crud_mongo.js
@@ -7,26 +7,25 @@ async function main() {
         console.log('Connected to MongoDB database');
 
         const db = client.db("defaultdb");
+        //ws masks
         const collection = db.collection('ws_masks');
+        insertIntoCollection(collection,{ id: '1', name: 'mask1', description: 'description1', mask_json: 'json1' })
 
-        // Create (Insert a document)
-        const doc = { id: '1', name: 'mask1', description: 'description1', mask_json: 'json1' };
-        const result = await collection.insertOne(doc);
-        console.log(`Inserted document with _id: ${result.insertedId}`);
+        findInCollectionByQuery(collection,{id :'1'})
 
-        // Read (Find a document)
-        const query = { id: '1' };
-        const foundDoc = await collection.findOne(query);
-        console.log(`Found document: ${JSON.stringify(foundDoc)}`);
+        updateInCollection(collection,{id :'1'},{ $set: { name: 'updated mask' } })
 
-        // Update (Update a document)
-        const update = { $set: { name: 'updated mask' } };
-        const updateResult = await collection.updateOne(query, update);
-        console.log(`Updated ${updateResult.modifiedCount} document(s)`);
+        deleteInCollection(collection,{id :'1'})
 
-        // Delete (Delete a document)
-        const deleteResult = await collection.deleteOne(query);
-        console.log(`Deleted ${deleteResult.deletedCount} document(s)`);
+        //ws entries
+        const collectionEntries= db.collection('ws_entries');
+        insertIntoCollection(collectionEntries,{ id: '1', id_mask: '1',  entry_json: {test:'test'} })
+
+        findInCollectionByQuery(collectionEntries,{id :'1'})
+
+        updateInCollection(collection,{id :'1'},{ $set: { entry_json: {test:'test'} } })
+
+        deleteInCollection(collectionEntries,{id :'1'})
 
     } catch (err) {
         console.error('Error performing CRUD operations', err);
@@ -34,3 +33,23 @@ async function main() {
 }
 
 main();
+
+async function insertIntoCollection(collectionName, data){
+    const result = await collectionName.insertOne(data);
+    console.log(`Inserted document with _id: ${result.insertedId}`);
+}
+
+async function findInCollectionByQuery(collectionName, query){
+    const foundDoc = await collectionName.findOne(query);
+    console.log(`Found document: ${JSON.stringify(foundDoc)}`);
+}
+
+async function updateInCollection(collectionName, query, update){
+    const updateResult = await collectionName.updateOne(query, update);
+    console.log(`Updated ${updateResult.modifiedCount} document(s)`);
+}
+
+async function deleteInCollection(collectionName, query){
+    const deleteResult = await collectionName.deleteOne(query);
+    console.log(`Deleted ${deleteResult.deletedCount} document(s)`);
+}
